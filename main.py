edcc59ded3b55bae1e2c46c88eb70915c8c44e0d
umar <aaljabbar.aj@gmail.com>
2021-06-20 00:24:24 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 
 try:
-    r = requests.get('https://go ogle.com')
+    r = requests.get('https://google.com')
     if r.status_code == 200:
         print(r.text)
 except Exception as e:
     print('error : ', e)
+
+print('haha')
